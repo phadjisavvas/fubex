ce68173367d9b928fb63e30f3633505389f13572
unknown <panayiotishadj23@hotmail.com>
2025-07-06 20:27:42 +0300

    Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,5 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jashplgrqeeewlrchnjjh.supabase.io';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imphc2hwbGdycWVld2xyY2huamhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MTA3NjcsImV4cCI6MjA2NzM4Njc2N30.hWoBtWNaauOObiQHiMxg5Nqq8AA87KDmdZ4cV66ppnM'; // Get this from Supabase -> Project -> API page
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://jashplgrqeewlrchnjhh.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
