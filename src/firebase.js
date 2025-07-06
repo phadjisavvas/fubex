e87a6544c142cc5bbe586808d997faa87e16d47a
unknown <panayiotishadj23@hotmail.com>
2025-07-06 11:31:38 +0300

    firebase test

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,21 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Replace these with your real config values from Firebase Console
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC0Cb2CR8zGl5uG_uUGiUl2cw7TahEmQVI",
-  authDomain: "Fubex.firebaseapp.com",
+  authDomain: "fubex-ef833.firebaseapp.com",
   projectId: "fubex-ef833",
-  storageBucket: "fubex.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "1:64296286089:web:e0dc18ff37cf81207f5358"
+  storageBucket: "fubex-ef833.firebasestorage.app",
+  messagingSenderId: "64296286089",
+  appId: "1:64296286089:web:e0dc18ff37cf81207f5358",
+  measurementId: "G-T522R2J0H7"
 };
 
-// Initialize Firebase App
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export Firebase Auth instance to use in other files
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
