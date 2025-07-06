9bf3d31aff75c81c0f49a5c0b5a69336d56a00a2
unknown <panayiotishadj23@hotmail.com>
2025-07-06 19:49:59 +0300

    Supabase

--- src/fubex.jsx
+++ src/fubex.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import supabase from "./supabase";
+import { supabase } from './supabase';
 
 const addTask = async () => {
   if (!newTask.task || !newTask.assigned || !newTask.due) return;
--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
+// src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jashplgrqeeewlrchnjjh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imphc2hwbGdycWVld2xyY2huamhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MTA3NjcsImV4cCI6MjA2NzM4Njc2N30.hWoBtWNaauOObiQHiMxg5Nqq8AA87KDmdZ4cV66ppnM'; // replace with actual anon key
+const supabaseUrl = 'https://jashplgrqeeewlrchnjjh.supabase.io';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imphc2hwbGdycWVld2xyY2huamhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MTA3NjcsImV4cCI6MjA2NzM4Njc2N30.hWoBtWNaauOObiQHiMxg5Nqq8AA87KDmdZ4cV66ppnM'; // Get this from Supabase -> Project -> API page
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseKey);
