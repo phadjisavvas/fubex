8eec635e85a120ffaf77c5c0e871e60bfe749ba8
unknown <panayiotishadj23@hotmail.com>
2025-07-06 17:37:50 +0300

    Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jashplgrqeeewlrchnjjh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imphc2hwbGdycWVld2xyY2huamhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MTA3NjcsImV4cCI6MjA2NzM4Njc2N30.hWoBtWNaauOObiQHiMxg5Nqq8AA87KDmdZ4cV66ppnM'; // Replace this with your actual anon key
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+const supabaseUrl = 'https://jashplgrqeewlrchnjhh.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
