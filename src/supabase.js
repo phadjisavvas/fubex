3d7d5d367336212677fd8c85221b6f5d23b5d3b0
unknown <panayiotishadj23@hotmail.com>
2025-07-07 19:34:38 +0300

    Use VITE_SUPABASE_KEY for Supabase client

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://jashplgrqeewlrchnjhh.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
