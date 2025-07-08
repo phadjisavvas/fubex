02b108844c6d524f42bdf1a5d7321924ef43c490
unknown <panayiotishadj23@hotmail.com>
2025-07-08 18:15:33 +0300

    Fixing backend/frontend

--- server/index.js
+++ server/index.js
@@ -41,4 +41,5 @@ app.post('/create-checkout-session', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
